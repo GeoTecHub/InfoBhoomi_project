1e634bc33da851c4197ee836466b57e8da8d372a
Delz <73336938+KD-96@users.noreply.github.com>
2024-07-18 18:14:52 +0530

    add tag panel

--- src/app/Map-Components/map/map.component.ts
+++ src/app/Map-Components/map/map.component.ts
@@ -6,25 +6,45 @@ import {
   ElementRef,
   Input,
 } from '@angular/core';
-import { OpenLayerService } from '../../openLayerServices/open-layer.service';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import { Feature } from 'ol';
 import { Geometry } from 'ol/geom';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+import { OpenLayerService } from '../../openLayerServices/open-layer.service';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { TagPanelPopupComponent } from '../popup-windows/tag-panel-popup/tag-panel-popup.component';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDialogModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css',
 })
 export class MapComponent implements OnInit, AfterViewInit {
   @ViewChild('mapElement') mapElementRef!: ElementRef;
 
-  constructor(private openLayerService: OpenLayerService) {}
+  constructor(
+    private openLayerService: OpenLayerService,
+    public dialog: MatDialog
+  ) {}
+  // !--------------------------------
+  opentagPanelPopup() {
+    const dialogRef = this.dialog.open(TagPanelPopupComponent, {
+      minWidth: '600px',
+      maxWidth: '800px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  // !--------------------------------
   ngOnInit(): void {}
   ngAfterViewInit(): void {
     this.openLayerService.initializeMap(this.mapElementRef.nativeElement);
--- src/app/Map-Components/popup-windows/tag-panel-popup/tag-panel-popup.component.ts
+++ src/app/Map-Components/popup-windows/tag-panel-popup/tag-panel-popup.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tag-panel-popup',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './tag-panel-popup.component.html',
   styleUrl: './tag-panel-popup.component.css',
 })
